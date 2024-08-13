bb390aaad773d35ba31a424c8ea472da6271ac2f
Addy Rodriguez <rdz.33.a@gmail.com>
2024-08-13 14:31:11 -0500

    Part 2

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+// Part 1
+// Select and cache the <main> element in a variable named mainEl
 const mainEl = document.querySelector('main')
 
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
 mainEl.style.backgroundColor = 'var(--main-bg)'
 
+// Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
 mainEl.innerHTML =  '<h1>DOM Manipulation</h1>'
 
+// Add a class of flex-ctr to mainEl.
 mainEl.classList.add('flex-ctr')
 
+// Part 2
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById('top-menu')
+
+// Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = '100%'
+
+// Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+// Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add('flex-around')
