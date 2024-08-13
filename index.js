ba0c2efdf563ea967fcc39c221ce8ca71deca1a9
Addy Rodriguez <rdz.33.a@gmail.com>
2024-08-13 14:16:54 -0500

    Part 1

---
+++ index.js
@@ -0,0 +1,8 @@
+const mainEl = document.querySelector('main')
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML =  '<h1>DOM Manipulation</h1>'
+
+mainEl.classList.add('flex-ctr')
+
